d02928dde933109548bb2094725ba545585f061f
aaenyx2 <aaenyx2@mail.dgist.ac.kr>
2018-08-07 06:54:02 +0900

    친구 목록 클릭 시 해당 칸으로 이동

--- app/src/main/java/com/dgistalk/dgtalk/LoginActivity.java
+++ app/src/main/java/com/dgistalk/dgtalk/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signOut();
+        //firebaseAuth.signOut();
         String splash_background = firebaseRemoteConfig.getString(getString(R.string.rc_color));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
